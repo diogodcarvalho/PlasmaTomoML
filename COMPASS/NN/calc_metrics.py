3ac048dd8f0df795c01e3537927fa5472fbc2038
diogodcarvalho <diogocarvalho2009@hotmail.com>
2018-08-24 22:39:49 +0100

    added COMPASS/NN/calc_metrics.py

--- COMPASS/NN/calc_metrics.py
+++ COMPASS/NN/calc_metrics.py
@@ -1,6 +1,6 @@
 
+import csv
 import numpy as np 
-import matplotlib.pyplot as plt
 
 import sys
 sys.path.insert(0, '../bib/')
@@ -26,42 +26,53 @@ print 'ef:', ef.shape, ef.dtype
 save_path = './Results/'
 indeces = np.load( save_path + './i_divided.npz')
 
-g_valid = g[indeces['i_valid']]
-f_valid = f[indeces['i_valid']]
-ef_valid = ef[indeces['i_valid']]
-pulse_valid = pulse[indeces['i_valid']]
-t_valid = t[indeces['i_valid']]
-chi2_valid = chi2[indeces['i_valid']]
+g_test = g[indeces['i_valid']]
+f_test = f[indeces['i_valid']]
+ef_test = ef[indeces['i_valid']]
+pulse_test = pulse[indeces['i_valid']]
+t_test = t[indeces['i_valid']]
+chi2_test = chi2[indeces['i_valid']]
 
-print 'g_valid:', g_valid.shape, g_valid.dtype
-print 'f_valid:', f_valid.shape, f_valid.dtype
-print 'ef_valid:', ef_valid.shape, ef_valid.dtype
+print 'g_test:', g_test.shape, g_test.dtype
+print 'f_test:', f_test.shape, f_test.dtype
+print 'ef_test:', ef_test.shape, ef_test.dtype
 
-# -------------------------------------------------------------------------
-# Import matrix and perform new reconstructions (only in validation set)
-# Choose the directory in wich it was stored
-# output files of this script will also be stored there
+#------------------------------------------------------------------
+# Initialize NN for given trained weights
 
-save_path = './Results/'
-M = np.load(save_path + 'M.npy')
+import nn_model
 
-g_m = np.dot(M,f_valid.transpose()).transpose()
-g_m = g_m.reshape((g_m.shape[0],bib_geom.N_ROWS,bib_geom.N_COLS))
-print 'g_m :', g_m.shape, g_m.dtype
+# recover number of filters the NN was trained with
+with open(save_path + 'model_options.log') as inputfile:
+ 	for row in csv.reader(inputfile):
+ 		print row[0]
+ 		if 'filters' in row[0]:
+ 			filters = int(row[0][9:])
 
-# Uncomment if you decide to clip mininimum value of images to zero
-# g_valid = np.clip(g_valid, a_min = 0, a_max = None)
-# g_m = np.clip(g_m, a_min = 0, a_max = None)
+# build model
+model = nn_model.build_model(filters)
+
+# load trained parameters
+model_parameters = save_path + 'model_parameters.hdf'
+model.load_weights(model_parameters)
+
+# calculate new reconstructions with the NN
+g_test_nn = model.predict(f_test)
+# resize to original resolution
+g_test_nn = bib_utils.resize_NN_image(g_test_nn, training = False)
+
+print 'g_test:', g_test.shape, g_test.dtype
+print 'g_test_nn:', g_test_nn.shape, g_test_nn.dtype
 
 # ------------------------------------------------------------------
 # Image metrics
 
-ssim,mse,psnr, nrmse = bib_metrics.compare_all_metrics(g_valid,g_m, mean = False)
+ssim,mse,psnr, nrmse = bib_metrics.compare_all_metrics(g_test,g_test_nn, mean = False)
 
 # ------------------------------------------------------------------
 # Pixelwise Metrics
 
-rmse_pixel,amre_pixel,mre_pixel = bib_metrics.compare_all_metrics_pixel(g_valid,g_m, mean = True)
+rmse_pixel,amre_pixel,mre_pixel = bib_metrics.compare_all_metrics_pixel(g_test,g_test_nn, mean = True)
 
 bib_metrics.plot_metric_pixel(rmse_pixel, 'RMSE', save_path,vmin = 0, vmax = 0.4,clb_legend = 'kW m$^{-3}$')
 bib_metrics.plot_metric_pixel(mre_pixel, 'MRE', save_path,vmin = -.15, vmax = .15)
@@ -70,13 +81,13 @@ bib_metrics.plot_metric_pixel(amre_pixel, 'AMRE', save_path,vmin = 0, vmax = 0.3
 # ------------------------------------------------------------------
 # Total Power Emited Error
 
-e_power = (np.sum(g_valid,axis = (1,2)) - np.sum(g_m, axis = (1,2)))/np.sum(g_valid,axis = (1,2))
+e_power = (np.sum(g_test,axis = (1,2)) - np.sum(g_test_nn, axis = (1,2)))/np.sum(g_test,axis = (1,2))
 
 #------------------------------------------------------------------
 # Check Plasma Center
 
-R_centroid, Z_centroid  = bib_geom.get_centroid(g_valid)
-R_nn_centroid, Z_nn_centroid = bib_geom.get_centroid(g_m)
+R_centroid, Z_centroid  = bib_geom.get_centroid(g_test)
+R_nn_centroid, Z_nn_centroid = bib_geom.get_centroid(g_test_nn)
 
 R_error = np.abs(R_centroid-R_nn_centroid)*1e3
 Z_error = np.abs(Z_centroid-Z_nn_centroid)*1e3
@@ -85,7 +96,7 @@ Z_error = np.abs(Z_centroid-Z_nn_centroid)*1e3
 # Chi2 Values
 
 geom = bib_geom.Geometry(data_directory + 'tomo_GEOM.npz')
-chi2_m = geom.get_chi2(f_valid,g_m,ef_valid)
+chi2_m = geom.get_chi2(f_test,g_test_nn,ef_test)
 
 #------------------------------------------------------------------
 # Mean values
@@ -103,8 +114,6 @@ print 'chi2_m: %.2f +- %.2f' % (np.mean(chi2_m), np.std(chi2_m))
 #------------------------------------------------------------------
 # Save results to file *.csv for further analysis
 
-import csv
-
 print '\nCreating :', save_path + 'metrics.csv'
 
 with open(save_path + 'metrics.csv', 'w') as csvfile:
@@ -112,8 +121,8 @@ with open(save_path + 'metrics.csv', 'w') as csvfile:
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
 	writer.writeheader()
-	for i in range(pulse_valid.shape[0]):
-		writer.writerow({'pulse': pulse_valid[i], 't': t_valid[i] , 'ssim': ssim[i],
+	for i in range(pulse_test.shape[0]):
+		writer.writerow({'pulse': pulse_test[i], 't': t_test[i] , 'ssim': ssim[i],
 		 'psnr': psnr[i], 'nrmse': nrmse[i], 'e_power': e_power[i], 
 		 'R_error': R_error[i], 'Z_error': Z_error[i], 'chi2_m': chi2_m[i]})
 
@@ -125,14 +134,14 @@ with open(save_path + 'metrics.csv', 'w') as csvfile:
 # it seems this happens when there is saturation of detectors
 # which generates poor quality reconstructions 
 # these reconstructions should not taken into account
-chi2_2 = geom.get_chi2(f_valid,g_valid,ef_valid)
+chi2_2 = geom.get_chi2(f_test,g_test,ef_test)
 
 from collections import Counter
 
-if np.mean(np.abs(chi2_valid-chi2_2))>0.1:
+if np.mean(np.abs(chi2_test-chi2_2))>0.1:
 	print '\n-----------------ATTENTION---------------------'
 	print 'Original chi2 from MFR Matlab'
-	print 'chi2: %.5f +- %.5f' % (np.mean(chi2_valid), np.std(chi2_valid))  
+	print 'chi2: %.5f +- %.5f' % (np.mean(chi2_test), np.std(chi2_test))  
 	print 'Value calculated for same reconstructions but with bib_geom.py code'
 	print 'chi2_2: %.5f +- %.5f' % (np.mean(chi2_2), np.std(chi2_2))
 	print("""\nThe values of chi2_2 and chi2 should be similar
@@ -144,7 +153,7 @@ probably belong to events where detectors saturated.""")
 	# one way to identify such shots
 	# also checking the metrics.csv file might help
 	problematic_events = np.abs(chi2_2-1)> 0.1
-	problematic_pulses = pulse_valid[problematic_events]
+	problematic_pulses = pulse_test[problematic_events]
 	problems_dict = Counter(problematic_pulses)
 	print '\nProblematic pulses'
 	for pulse in problems_dict.keys():
